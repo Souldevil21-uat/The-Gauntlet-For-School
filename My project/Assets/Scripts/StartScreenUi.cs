5c7d42194e3e4888100979821515b38eb9e8a415
Souldevil21-uat <35380580+Souldevil21-uat@users.noreply.github.com>
2025-03-12 08:05:29 -0500

    Added a start Menu

---
+++ My project/Assets/Scripts/StartScreenUi.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenUI : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main Game"); // Change "GameScene" to your actual game scene name
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("OptionsMenu"); // Ensure you have an Options Menu scene
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
